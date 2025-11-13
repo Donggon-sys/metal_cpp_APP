7720958bbabb95dea32a6a85d1c77035e72dbc4e
Donggon-sys <63091200+Donggon-sys@users.noreply.github.com>
2025-11-13 13:48:15 +0800

    commit init

---
+++ metal_cpp_APP/src/main.mm
@@ -0,0 +1,13 @@
+//
+//  main.mm
+//  metal_cpp_APP
+//
+//  Created by Chenruyi on 2025/11/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "打开窗口" << std::endl;
+    return 0;
+}
